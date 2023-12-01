2e4b9a8c87eaf59789ceb07eda1b371dcbf8f99a
Marina Cullen <youfoundmarina@gmail.com>
2023-12-01 10:56:16 -0800

    created shared stories project
    just setting up all the required code to start the project, imports and dependencies.

---
+++ sharedStories/src/main/java/com/mcullen/stories/SharedStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.mcullen.stories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedStoriesApplication.class, args);
+	}
+
+}
